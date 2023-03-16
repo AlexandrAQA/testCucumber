e01bc10797774f265566ec207be4ef7b8fac1176
Alex Panaev <panaev.as@gmail.com>
2023-03-16 21:23:35 +0400

    Refactoring OnlinerSearchPage & OnlinerSearchSteps

--- src/main/java/com/aqa/steps/OnlinerSearchSteps.java
+++ src/main/java/com/aqa/steps/OnlinerSearchSteps.java
@@ -1,5 +1,6 @@
 package com.aqa.steps;
 
+import com.aqa.pages.OnlinerSearchPage;
 import com.codeborne.selenide.ElementsCollection;
 import io.cucumber.java.en.Given;
 import io.cucumber.java.en.Then;
@@ -8,28 +9,25 @@ import io.cucumber.java.en.When;
 import static com.codeborne.selenide.Selenide.*;
 import static org.assertj.core.api.Assertions.assertThat;
 
-public class OnlinerStepsDefinition {
+public class OnlinerSearchSteps {
+
+    OnlinerSearchPage searchPage = new OnlinerSearchPage();
 
     @Given("User is on {string} page")
-    public void user_is_on_page(String pageAddress) {
-        //open(pageAddress);
+    public void userIsOnPage(String pageAddress) {
         open(pageAddress);
     }
 
     @When("User searches for {string}")
-    public void user_searches_for(String searchText) {
-        $x("//input[@class='fast-search__input']")
-                .sendKeys(searchText);
+    public void userSearchesFor(String searchText) {
+        searchPage.searchInCatalog(searchText);
     }
 
     @Then("{string} is the first search result")
     public void isTheFirstSearchResult(String searchResultTitle) {
-        switchTo().frame($(".modal-iframe"));//iframe and css
-        ElementsCollection productTitles =
-                $$(".result__item_product .product__title .product__title-link");
-//        System.out.println();
-        String actualTitle = productTitles.first()
-                                          .getText();
+        ElementsCollection productTitles = searchPage.getProductTitles();
+
+        String actualTitle = productTitles.first().getText();
         assertThat(actualTitle)
                 .as("The first searchResult is incorrect")
                 .contains(searchResultTitle);
