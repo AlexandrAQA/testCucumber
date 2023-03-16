fbfeaa10dd0e42be1f3bad65a02815dc773195b4
Alex Panaev <panaev.as@gmail.com>
2023-03-16 12:02:26 +0400

    added searching elements into iFrame and checking first

--- src/main/java/com/aqa/steps/OnlinerStepsDefinition.java
+++ src/main/java/com/aqa/steps/OnlinerStepsDefinition.java
@@ -1,32 +1,38 @@
 package com.aqa.steps;
 
+import com.codeborne.selenide.ElementsCollection;
 import io.cucumber.java.en.Given;
 import io.cucumber.java.en.Then;
 import io.cucumber.java.en.When;
 
-import static com.codeborne.selenide.Selenide.$x;
-import static com.codeborne.selenide.Selenide.open;
+import static com.codeborne.selenide.Selenide.*;
+import static org.assertj.core.api.Assertions.assertThat;
 
 public class OnlinerStepsDefinition {
 
     @Given("User is on {string} page")
     public void user_is_on_page(String pageAddress) {
         //open(pageAddress);
-        open("https://www.onliner.by/");
-        //throw new io.cucumber.java.PendingException();
+        open(pageAddress);
     }
+
     @When("User searches for {string}")
     public void user_searches_for(String searchText) {
         $x("//input[@class='fast-search__input']")
                 .sendKeys(searchText);
-       // throw new io.cucumber.java.PendingException();
     }
+
     @Then("{string} is the first search result")
-    public void is_the_first_search_result(String string) {
-
-       // throw new io.cucumber.java.PendingException();
+    public void isTheFirstSearchResult(String searchResultTitle) {
+        switchTo().frame($(".modal-iframe"));//iframe and css
+        ElementsCollection productTitles =
+                $$(".result__item_product .product__title .product__title-link");
+//        System.out.println();
+        String actualTitle = productTitles.first()
+                                          .getText();
+        assertThat(actualTitle)
+                .as("The first searchResult is incorrect")
+                .contains(searchResultTitle);
     }
-
-
 }
 
