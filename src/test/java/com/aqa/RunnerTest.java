f94da519dad3a05d4d79791864d888d51b1c17fa
Alex Panaev <panaev.as@gmail.com>
2023-03-17 21:49:07 +0400

    added cucumber.properties & plugin pretty

--- src/test/java/com/aqa/RunnerTest.java
+++ src/test/java/com/aqa/RunnerTest.java
@@ -5,7 +5,9 @@ import io.cucumber.testng.CucumberOptions;
 
 @CucumberOptions(features = "src/test/resources/features",
                      glue = "com.aqa.steps",
-                     tags = "@smoke")
+                     tags = "@smoke",
+                     plugin = {"pretty","html:target/cucumber.html",
+                             "json:target/cucumber.html"})
 
 
 public class RunnerTest extends AbstractTestNGCucumberTests {
